9ada8d0df0e70d2afd55935b0c3f1f9a58ebc595
black-An0nym0us <130042127+black-An0nym0us@users.noreply.github.com>
2023-04-27 00:04:15 +0600

    Update random-anonymous.py

--- random-anonymous.py
+++ random-anonymous.py
@@ -1,5 +1,5 @@
-# Facebook: Tutul King
-# Github: Tutul-King
+# Facebook: sajib
+# Github: anonymous
 import os,sys,time,json,random,re,string,platform,base64,uuid
 os.system("git pull")
 from bs4 import BeautifulSoup as sop
